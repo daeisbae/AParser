3c42fd51a0225d3f8930fd999cf5dde915b8558c
Dae Is Bae <bigmart000918@gmail.com>
2024-01-26 14:44:36 +0000

    Fillout Token class methods and token generating function

--- token/token.cpp
+++ token/token.cpp
@@ -1,1 +1,36 @@
+#include "token.hpp"
 
+Token::Token::Token(std::shared_ptr<File> src_file, const std::string& input, int char_position, int char_line, TokenType toktype, OperatorPtr op=OperatorPtr(nullptr)) {
+    this->srcfile = src_file;
+    this->input = input;
+    this->ch_position = char_position;
+    this->ch_line = char_line;
+    this->toktype = toktype;
+    this->op = op;
+}
+
+Token::~Token() {
+
+}
+
+Token::TokenType TokenType() const {
+    return toktype;
+}
+
+int CharPosition() const {
+    return ch_position;
+}
+
+int CharLine() const {
+    return ch_line;
+}
+
+std::string Text() const {
+    return input;
+}
+
+TokenPtr GenerateToken(std::shared_ptr<File> src_file, const std::string& input, int char_position, int char_line,
+                         TokenType toktype, OperatorPtr op=OperatorPtr(nullptr)) {
+    return TokenPtr(new Token(std::shared_ptr<File> src_file, const std::string& input, int char_position, int char_line,
+                             TokenType toktype, OperatorPtr op=OperatorPtr(nullptr)))
+}

75f1eb0c1296b800e1189956b821a62ccc47556c
Dae Is Bae <bigmart000918@gmail.com>
2024-01-30 11:01:55 +0000

    Update what file to delete during execution

--- rmcmake.sh
+++ rmcmake.sh
@@ -1,4 +1,11 @@
+# Remove CMake generated files
 find . -iwholename '*cmake*' -not -name CMakeLists.txt -and -not -name rmcmake.sh -and -not -name cmake-single-platform.yml -delete
-find . -name Makefile -delete
-find . -name AParser -delete
 find . -name 'lib*.a' -delete
+find . -name Makefile -delete
+
+# Remove Executable
+find . -name AParser -delete
+find . -name test_stringutil -delete
+
+# Remove gtest package
+find . -name '_deps' -type d -exec rm -r {} +

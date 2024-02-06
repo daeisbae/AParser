56eedfaaaedb9212140486524f3d7f8c220c9c86
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2024-02-06 21:40:44 +0900

    Add ConvertStringToQueue for lexer

--- stringutil/stringutil.hpp
+++ stringutil/stringutil.hpp
@@ -1,6 +1,7 @@
 #ifndef STRINGUTIL_H
 #define STRINGUTIL_H
 
+#include <queue>
 #include <string>
 
 // It's a function that works like split() in python.
@@ -8,11 +9,15 @@
 // Delimits the "\n" keyword to split the next line.
 std::string ExtractTextOfLine(std::string text, const int line);
 
+std::string ConvertCharToString(char ch);
+
+std::queue<char> ConvertStringToQueue(std::string text);
+
 class LineOutOfBoundException : public std::exception {
-public:
-    const char* what() const noexcept override {
-        return "Line number is out of the bound.";
-    }
+ public:
+  const char* what() const noexcept override {
+    return "Line number is out of the bound.";
+  }
 };
 
 #endif

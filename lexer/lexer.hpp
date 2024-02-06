f334a7851b2237c3d8fab7b26d45a3031ec470f0
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2024-02-06 23:35:22 +0900

    Change lexer read from string to queue<char>

--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -1,45 +1,72 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#include <vector>
+#include <queue>
 #include <string>
+#include <vector>
 
-#include "token.hpp"
 #include "file.hpp"
+#include "operator.hpp"
+#include "token.hpp"
 
 class Lexer {
-private:
-    FilePtr file;
-    std::string text;
-    int currPos;
-    int nextPos;
-    int line;
-    char currCh;
+ private:
+  FilePtr file;
+  std::queue<char> textqueue;
+  int line;
 
+  // Parse the string
+  std::string readStr();
+  // Parse the number
+  std::string readNum();
+  // Parse the literal (variable/function naming)
+  std::string readLiteral();
 
-    void moveNextPos();
-    char peek();
-    void readCh();
-    // Parse the string
-    std::string readStr();
-    // Parse the number
-    std::string readNum();
-    // Parse the literal (variable/function naming)
-    std::string readLiteral();
+  std::string readWhitespace();
 
-public:
-    Lexer(std::string input);
-    Lexer(FilePtr fileptr);
-    ~Lexer();
+ public:
+  Lexer(std::string input);
+  Lexer(FilePtr fileptr);
+  ~Lexer();
 
-    TokenPtr NextToken();
+  // Return the keyword into tokentype
+  TokenType GetReservedKeywordTokenType(std::string keyword) const;
+
+  // Return the next keyword in token pointer (TokenPtr)
+  TokenPtr NextToken();
+
+  // If there's file member, read next line.
+  void ReadNextLine();
 };
 
 class WrongLexingException : public std::exception {
-public:
-    const char* what() const noexcept override {
-        return "Unexpected value in the line.";
-    }
+ private:
+  std::string errinfo;
+
+ public:
+  WrongLexingException(std::string err) : errinfo(err){};
+
+  const char* what() const noexcept override { return errinfo.c_str(); }
+};
+
+class LexLineOutOfBoundException : public std::exception {
+ private:
+  std::string errinfo;
+
+ public:
+  LexLineOutOfBoundException(std::string err) : errinfo(err){};
+
+  const char* what() const noexcept override { return errinfo.c_str(); }
+};
+
+class NullFileMemberException : public std::exception {
+ private:
+  std::string errinfo;
+
+ public:
+  NullFileMemberException(std::string err) : errinfo(err){};
+
+  const char* what() const noexcept override { return errinfo.c_str(); }
 };
 
 #endif

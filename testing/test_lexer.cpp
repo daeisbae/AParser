f3d61ea6ee2cf9b5c448e2fcc1980656d9dfe4e3
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2024-02-07 13:30:51 +0900

    Add test cases for function lexing

--- testing/test_lexer.cpp
+++ testing/test_lexer.cpp
@@ -6,7 +6,7 @@
 #include "operator.hpp"
 #include "token.hpp"
 
-TEST(Lexer, AssignIntegerToVariable) {
+TEST(LexerTest, AssignIntegerToVariable) {
   Lexer test1 = Lexer("set hello = 2");
 
   // 1
@@ -42,4 +42,77 @@ TEST(Lexer, AssignIntegerToVariable) {
   // 8
   EXPECT_EQ(*(test1.NextToken()),
             *(GenerateToken("", TokenType::EOL, OperatorPtr(nullptr))));
+}
+
+TEST(LexerTest, Function) {
+  Lexer test1 = Lexer("set var1 = func(){ return 123 }");
+
+  // 1
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken("set", TokenType::SET, OperatorPtr(nullptr))));
+
+  // 2
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken(" ", TokenType::WHITESPACE, OperatorPtr(nullptr))));
+
+  // 3
+  EXPECT_EQ(*(test1.NextToken()), *(GenerateToken("var1", TokenType::IDENTIFIER,
+                                                  OperatorPtr(nullptr))));
+
+  // 4
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken(" ", TokenType::WHITESPACE, OperatorPtr(nullptr))));
+
+  // 5
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken("=", TokenType::OPERATOR,
+                            GenerateOp("=", OperatorType::EQUAL))));
+
+  // 6
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken(" ", TokenType::WHITESPACE, OperatorPtr(nullptr))));
+
+  // 7
+  EXPECT_EQ(*(test1.NextToken()), *(GenerateToken("func", TokenType::FUNCTION,
+                                                  OperatorPtr(nullptr))));
+
+  // 8
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken("(", TokenType::OPERATOR,
+                            GenerateOp("(", OperatorType::L_PARENTHESIS))));
+
+  // 9
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken(")", TokenType::OPERATOR,
+                            GenerateOp(")", OperatorType::R_PARENTHESIS))));
+
+  // 10
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken("{", TokenType::OPERATOR,
+                            GenerateOp(")", OperatorType::L_BRACE))));
+
+  // 11
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken(" ", TokenType::WHITESPACE, OperatorPtr(nullptr))));
+
+  // 12
+  EXPECT_EQ(*(test1.NextToken()), *(GenerateToken("return", TokenType::RETURN,
+                                                  OperatorPtr(nullptr))));
+
+  // 13
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken(" ", TokenType::WHITESPACE, OperatorPtr(nullptr))));
+
+  // 14
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken("123", TokenType::INTEGER, OperatorPtr(nullptr))));
+
+  // 15
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken(" ", TokenType::WHITESPACE, OperatorPtr(nullptr))));
+
+  // 16
+  EXPECT_EQ(*(test1.NextToken()),
+            *(GenerateToken("}", TokenType::OPERATOR,
+                            GenerateOp("}", OperatorType::R_BRACE))));
 }

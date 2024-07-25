9a3f9489071618da749a60ac7bf008dd76e867be
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2024-07-25 11:36:36 -0700

    [#107] Implement code docs for stringutil class in doxygen format

--- stringutil/stringutil.hpp
+++ stringutil/stringutil.hpp
@@ -1,11 +1,25 @@
+/**
+ * @file stringutil.hpp
+ * @brief Contains the string utility functions
+ */
 #ifndef STRINGUTIL_H
 #define STRINGUTIL_H
 
 #include <queue>
 #include <string>
 
+/**
+ * @brief Convert a char to a string
+ * @param ch The char to convert
+ * @return The string representation of the char
+ */
 std::string ConvertCharToString(char ch);
 
+/**
+ * @brief Convert a string to a queue of chars
+ * @param text The string to convert
+ * @return The queue representation of the string
+ */
 std::queue<char> ConvertStringToQueue(std::string text);
 
 #endif

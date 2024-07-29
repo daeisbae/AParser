a5b1eed0adf6041481034d7337756fce1056734c
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2024-07-29 11:30:26 -0700

    [#107] Implement code docs for parser class in doxygen format

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file parser.hpp
+ * @brief Contains the Parser class and its helper classes.
+ * Converts the list of Tokens to AST nodes (Statement and Expression).
+ */
 #ifndef PARSER_H
 #define PARSER_H
 
@@ -6,53 +11,109 @@
 #include "ast.hpp"
 #include "token.hpp"
 
+/**
+ * @brief The Parser class that takes in a queue of Token and produces an AST Statement and Expression
+ */
 class Parser {
  private:
   std::queue<TokenPtr> tok_queue_;
 
-  // Preview the next token
+  /**
+   * @brief Preview the next token
+   */
   TokenPtr Peek();
 
-  // Check if the token matches the expected TokenType | OperatorType
-  // If it match, return TokenPtr
-  // Else throw Exception
+  /**
+   * @brief Check if the next token is the expected TokenType
+   * @param expected_tok_type the expected TokenType
+   * @return TokenPtr the next token
+   */
   TokenPtr ExpectedTokenType(TokenType expected_tok_type);
+
+  /**
+   * @brief Check if the next token is the expected OperatorType
+   * @param expected_op_type the expected OperatorType
+   * @return TokenPtr the next token
+   */
   TokenPtr ExpectedTokenType(OperatorType expected_op_type);
 
-  // Return the peeked value and pops the tok_queue
+  /**
+   * @brief Return the peeked value and pops the tok_queue (Token Queue)
+   * @return TokenPtr the next token
+   */
   TokenPtr Eat();
 
+  /**
+   * @brief Parse the program
+   * @return Program the AST Program
+   */
   StatementPtr ParseStatement();
+  /**
+   * @brief Parse the Expression
+   * @return The Expression parsed
+   */
   ExpressionPtr ParseExpression();
 
-  // Reads the token and assign the value as expression
+  /**
+   * @brief Reads the token and assign the value as expression
+   * @return ExpressionPtr the expression parsed
+   */
   ExpressionPtr ParsePrimaryExpression();
 
-  // + and - operations
+  /**
+   * @brief Parse the addition and subtraction operations
+   * @return ExpressionPtr the expression parsed
+   */
   ExpressionPtr ParseAdditionExpression();
 
-  // * and / operations
+  /**
+   * @brief Parse the multiplication and division operations
+   * @return ExpressionPtr the expression parsed
+   */
   ExpressionPtr ParseMultiplicationExpression();
 
-  // Read whitespaces if it exists
+  /**
+   * @brief Parse the whitespaces
+   * @return ExpressionPtr the expression parsed
+   */
   ExpressionPtr ParseWhitespaceExpression();
 
-  // Declaration Identifier
+  /**
+   * @brief Parse the identifier declaration (Refer: Evaluater::EvaluateDefiningIdentifierExpression)
+   * @return StatementPtr the statement parsed
+   */
   StatementPtr ParseIdentifierDeclarationExpression();
 
-  // Assignment Identifier
+  /**
+   * @brief Parse the identifier assignment (Refer: Evaluater::EvaluateAssignIdentifierExpression)
+   * @return ExpressionPtr the statement parsed
+   */
   ExpressionPtr ParseIdentifierAssignmentExpression();
 
-  // Comparison (== , !=)
+  /**
+   * @brief Parse the comparison operations
+   * @return ExpressionPtr the expression parsed
+   */
   ExpressionPtr ParseComparisonExpression();
 
  public:
+  /**
+   * @brief Construct a new Parser object
+   */
   Parser();
   ~Parser();
 
+  /**
+   * @brief Convert the List of Tokens to List of AST nodes(Statement and Expression).
+   * @param tokenQueue the Token queue to be converted to list of AST nodes (Statement and Expression)
+   * @return Program the list of AST (Statements and Expressions)
+   */
   Program ProduceAST(std::queue<TokenPtr> &tokenQueue);
 };
 
+/**
+ * @brief The UnexpectedTokenParsedException class thrown when an Token is expected but another Token is parsed
+ */
 class UnexpectedTokenParsedException : public std::exception {
  private:
   std::string err_info_;
